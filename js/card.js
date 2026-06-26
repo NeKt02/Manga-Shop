4fc30ab583951f98143fc532f389138f3923c21a
Nekt02 <valentina22111989@gmail.com>
2026-06-26 19:04:14 +0300

    css

--- js/card.js
+++ js/card.js
@@ -15,24 +15,37 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         function displayProductDetails(product) {
             const productContainer = document.querySelector(".card");
-            productContainer.innerHTML = `<div class="card">
-            <div class="card-img">
-                <img src="${product.image}" alt="${product.title}">
-            </div>
-            <div class="card-title">${product.title}</div>
-            <div class="card-author">${product.author}</div>
-            <div class="card-footer">
-                <div class="card-price">${product.price} $</div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path>
-                        <path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path>
-                        <path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path>
-                        <path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path>
-                    </svg>
-                </button>
-            </div>
-            <p class="id" style="display:none;">${product.id}</p>
-        </div>`
+            productContainer.innerHTML = `
+<div class="product-page">
+            <a href="index.html" class="back-link">← Назад</a>
+    <div class="product-image">
+        <img src="${product.image}" alt="${product.title}">
+    </div>
+
+    <div class="product-info">
+
+        <h1>${product.title}</h1>
+
+        <p class="author">${product.author}</p>
+
+        <div class="price">${product.price}$</div>
+
+        <button class="buy-btn">
+            🛒 Купить
+        </button>
+
+        <div class="line"></div>
+
+        <h2>Описание</h2>
+
+        <p class="description">
+            ${product.description}
+        </p>
+
+    </div>
+
+</div>
+`;
         }
 
         displayProductDetails(await loadCartByID(savedId));
